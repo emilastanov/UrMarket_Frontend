78c5b282122dc4c255b0623a8c34b2e7008f3a7d
emilastanov <emila1998@me.com>
2021-09-26 13:22:52 +0300

    Removed unused component

--- src/pages/OfferDetails/index.js
+++ src/pages/OfferDetails/index.js
@@ -3,8 +3,6 @@ import {useParams} from "react-router-dom";
 
 import {getOffer_reducer} from "./reducer";
 
-import Button from "../../components/Button";
-
 import {Done} from "../../icons";
 
 import './style.css';
