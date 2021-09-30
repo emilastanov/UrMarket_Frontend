9abbd3032a988070e857acc248f97402c12ada6c
emilastanov <emila1998@me.com>
2021-09-30 13:33:50 +0300

    Updated dialog window

--- src/App/index.js
+++ src/App/index.js
@@ -39,34 +39,23 @@ const App = props => {
             });
     },[country])
 
-    const fallback = () => (
-        <div style={{
-            position: "absolute",
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            margin: "auto",
-            width: 100,
-            height: 50
-        }}>...</div>
-    );
+
 
     return <Router>
         <PageLoader state={isLoading}/>
         <Switch>
             <Route path="/admin">
-                <Suspense fallback={fallback}>
+                <Suspense fallback={""}>
                     {token ? <AdminPanel loader={setIsLoading}/> : <Login loader={setIsLoading}/>}
                 </Suspense>
             </Route>
             <Route path="/404">
-                <Suspense fallback={fallback}>
+                <Suspense fallback={""}>
                     <Page404 loader={setIsLoading}/>
                 </Suspense>
             </Route>
             <Route path="/:market">
-                <Suspense fallback={fallback}>
+                <Suspense fallback={""}>
                     <Main language="ru" loader={setIsLoading} country={country}/>
                 </Suspense>
             </Route>
